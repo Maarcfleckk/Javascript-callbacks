0020cf4ba83f4eecb40f757b1c385aa365593adf
FLEKI <123004635+Maarcfleckk@users.noreply.github.com>
2023-12-18 19:47:07 +0100

    Update helpers/validateString.js
    Co-authored-by: Zilong Zhan <144822890+ZilongZhan@users.noreply.github.com>

--- helpers/validateString.js
+++ helpers/validateString.js
@@ -7,6 +7,8 @@ function validateString(value) {
   if (typeof value !== "string" || value === "" || value === undefined) {
     return false;
   }
+  
   return true;
 }
+
 export default validateString;
